ddee849ea709d07e857f15732b679cf22cfd5c35
Yefei <macrofly@gmail.com>
2020-05-13 13:45:26 -0700

    BufferPool fetchPage API

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -37,12 +37,45 @@ BufferPoolManager::~BufferPoolManager() {
 Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
   // 1.     Search the page table for the requested page (P).
   // 1.1    If P exists, pin it and return it immediately.
+  latch_.lock();  // TODO could use a scoped lock to reduce the chance of not having an unlock call
+  if (page_table_.find(page_id) != page_table_.end()) {
+    page_id_t frame_id = page_table_[page_id];
+    pages_[frame_id].pin_count_ += 1;
+    latch_.unlock();
+    return &pages_[frame_id];
+  }
+
   // 1.2    If P does not exist, find a replacement page (R) from either the free list or the replacer.
   //        Note that pages are always found from the free list first.
-  // 2.     If R is dirty, write it back to the disk.
-  // 3.     Delete R from the page table and insert P.
-  // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
-  return nullptr;
+  frame_id_t free_frame_id = -1;
+  if (!free_list_.empty()) {
+    frame_id_t free_frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Victim(&free_frame_id)) {
+      latch_.unlock();
+      return nullptr;
+    }
+    // 2.     If R is dirty, write it back to the disk.
+    Page *replacedPage = &pages_[free_frame_id];
+    if (replacedPage->is_dirty_) {
+      FlushPageImpl(replacedPage->page_id_);
+      replacedPage->is_dirty_ = false;
+    }
+    // 3.     Delete R from the page table and insert P.
+    page_table_.erase(replacedPage->page_id_);
+  }
+
+  // 4.1   read in the page content from disk
+  pages_[free_frame_id].ResetMemory();
+  disk_manager_->ReadPage(page_id, pages_[free_frame_id].GetData());
+
+  // 4.2  Update P's metadata, and then return a pointer to P.
+  pages_[free_frame_id].page_id_ = page_id;
+  page_table_.insert({page_id, free_frame_id});
+  pages_[free_frame_id].pin_count_ = 1;
+
+  return &pages_[free_frame_id];
 }
 
 bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) { return false; }
